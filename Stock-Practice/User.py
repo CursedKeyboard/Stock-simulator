7498d153325c586b50c895883b44172ecd118961
Saiem Irfan <Saiem.irfan.2000@gmail.com>
2020-11-22 14:23:05 -0500

    Added helper methods in User for readability - Helper method used for readability to load data - Helper method used when purchasing stocks as it was not updating User   attributes as required before

--- Stock-Practice/User.py
+++ Stock-Practice/User.py
@@ -1,5 +1,5 @@
 import sqlite3
-from typing import List
+from typing import List, Tuple
 import dearpygui.core as dp
 
 
@@ -32,13 +32,13 @@ class User():
         """
         conn = sqlite3.connect(file_name)
         dp.log(f"Established connection to {file_name}")
-        self.share_by_name = {}
+        self.share_by_name, self.share_quantity = {}, {}
         for share in conn.execute("SELECT * FROM shares ORDER BY date"):
             share_name = share[0]
-            if share_name in self.share_by_name:
-                self.share_by_name[share_name].append(share[1:])
-            else:
-                self.share_by_name[share_name] = list(share[1:])
+            share_data = share[1:]
+            share_quantity = share_data[-1]
+            self.update_share_map(share_name, share_data)
+            self.update_share_count(share_name, share_quantity)
 
         user_data = conn.execute("SELECT * FROM data").fetchone()
         self.start_funds = user_data[0]
@@ -47,11 +47,33 @@ class User():
 
     def buy_share(self, amount: float, price: float, ticker: str, date: str, time: str) -> None:
         """ Buy <amount> shares for the User """
-        self.current_balance -= round(price * amount, 2)q
+        self.current_balance -= round(price * amount, 2)
+        self.current_balance = round(self.current_balance, 2)
+        share_data = (ticker, date, time, price, amount)
+
         conn = sqlite3.connect(self.file)
-        conn.execute(""" INSERT INTO shares VALUES (?, ?, ?, ?, ?)""", (ticker, date, time, price, amount))
-        print(self.current_balance)
+        conn.execute(""" INSERT INTO shares VALUES (?, ?, ?, ?, ?)""", share_data)
         print(conn.execute(""" SELECT balance FROM data """).fetchone())
         conn.execute(""" UPDATE data SET balance = ?""", (self.current_balance,))
         conn.commit()
+
+        self.update_share_count(ticker, amount)
+        self.update_share_map(ticker, share_data[1:])
+
         conn.close()
+
+    def update_share_count(self, share_name: int, share_count: float):
+        """ Update the share count of a ticker accordingly"""
+        if share_name in self.share_quantity:
+            self.share_quantity[share_name] += share_count
+            self.share_quantity[share_name] = round(self.share_quantity[share_name], 2)
+        else:
+            self.share_quantity[share_name] = share_count
+            self.share_quantity[share_name] = round(self.share_quantity[share_name], 2)
+
+    def update_share_map(self, share_name, share_data: Tuple[str, str, str, float, float]):
+        """ Update the share map of a user's owned shares accordingly"""
+        if share_name in self.share_by_name:
+            self.share_by_name[share_name].append(share_data)
+        else:
+            self.share_by_name[share_name] = [share_data]
